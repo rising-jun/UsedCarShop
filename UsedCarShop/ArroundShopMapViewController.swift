67492520e17818789a3c58bc41180c0aafccc30c
rising-jun <jun36817@naver.com>
2023-01-28 15:51:54 +0900

    add. ArroundShopMapViewController

---
+++ UsedCarShop/ArroundShopMapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  UsedCarShop
+//
+//  Created by 김동준 on 2023/01/28.
+//
+
+import SnapKit
+
+class ArroundShopMapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+private extension ArroundShopMapViewController {
+    func viewAttribute() {
+        
+    }
+    
+    func layout() {
+        
+    }
+}
