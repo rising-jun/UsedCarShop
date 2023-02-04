80f7185b16111b52b5de3e5e2c2a5a85084197f9
rising-jun <jun36817@naver.com>
2023-02-04 17:20:08 +0900

    add. car detail network feature

--- UsedCarShop/network/Service.swift
+++ UsedCarShop/network/Service.swift
@@ -5,10 +5,10 @@
 //  Created by 김동준 on 2023/01/28.
 //
 
-import RxSwift
+import Foundation
 
 final class Service {
-    func fetchMockCarShopJson(by api: TargetAPI) async -> Result<Data, NetworkError> {
+    func fetchMockJson(by api: TargetAPI) async -> Result<Data, NetworkError> {
         guard let url = api.makeCompleteURL() else { return .failure(.invailURL) }
         return await withCheckedContinuation({ [weak self] continuation in
             URLSession.shared.dataTask(with: url) { _, response, error in
@@ -17,16 +17,23 @@ final class Service {
                     return
                 }
                 if api.path == "/carshop" {
-                    guard let data = self.loadJSON(Service.self, fileName: "CarShop") else {
+                    guard let data = self.fetchMockCarShop() else {
                         return continuation.resume(returning: .failure(.notFound))
                     }
                     return continuation.resume(returning: .success(data))
                 }
                 if api.path == "/carinfo" {
-                    guard let data = self.loadJSON(Service.self, fileName: "CarInfo") else {
+                    print("hello?")
+                    guard let parameter = api.parameters,
+                          let id = parameter["id"] as? String else {
+                        return continuation.resume(returning: .failure(.notFound))
+                    }
+                    
+                    guard let data = self.fecthMockCarDetail(by: id) else {
                         continuation.resume(returning: .failure(.notFound))
                         return
                     }
+                    
                     continuation.resume(returning: .success(data))
                     return
                 }
@@ -39,8 +46,26 @@ final class Service {
             }.resume()
         })
     }
+}
+private extension Service {
+    func fetchMockCarShop() -> Data? {
+        guard let data = self.loadJSON(Service.self, fileName: "CarShop") else {
+            return nil
+        }
+        return data
+    }
     
-    private func loadJSON(_ loadClass: AnyClass, fileName: String) -> Data? {
+    func fecthMockCarDetail(by id: String) -> Data? {
+        guard let data = self.loadJSON(Service.self, fileName: "CarInfo") else {
+            return nil
+        }
+        let carDTOs = try? JSONDecoder().decode([CarDTO].self, from: data)
+        let findCarDTOs = carDTOs?.compactMap { $0 }.filter { $0.zones.contains { $0 == id } }
+        let jsonData = try? JSONEncoder().encode(findCarDTOs)
+        return jsonData
+    }
+    
+    func loadJSON(_ loadClass: AnyClass, fileName: String) -> Data? {
         guard let path = Bundle.init(for: loadClass).path(forResource: fileName, ofType: "json") else {
             print("--- file not found : \(fileName).json")
             return nil
--- UsedCarShop/scene/detailcarshop/DetailCarShopReactor.swift
+++ UsedCarShop/scene/detailcarshop/DetailCarShopReactor.swift
@@ -8,22 +8,51 @@
 import ReactorKit
 
 final class DetailCarShopReactor: Reactor {
-    init(carShopDTO: CarShopDTO) {
-        
-    }
-    
+    private let carShopDTO: CarShopDTO
+    var usecase = DetailCarShopUsecase()
     var initialState = State()
     
+    init(carShopDTO: CarShopDTO) {
+        self.carShopDTO = carShopDTO
+    }
+    
     enum Action {
-        
+        case viewDidLoad
     }
     
     enum Mutation {
-        
+        case updateCars([CarDTO])
     }
     
     struct State {
-        
+        var cars = [CarDTO]()
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .viewDidLoad:
+            return requestDetailCars().map { .updateCars($0) }
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .updateCars(let cars):
+            newState.cars = cars
+        }
+        return newState
     }
 }
-
+private extension DetailCarShopReactor {
+    func requestDetailCars() -> Observable<[CarDTO]> {
+        return Observable<[CarDTO]>.create { [weak self] observer in
+            guard let self = self else { return Disposables.create { } }
+            let task = Task {
+                let carshops = await self.usecase.requestDetailCars(by: self.carShopDTO.id)
+                observer.onNext(carshops ?? [])
+            }
+            return Disposables.create { task.cancel() }
+        }
+    }
+}
